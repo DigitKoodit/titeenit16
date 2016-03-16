cb4baa75156abc40bc731aaf0ac581022cc78f25
Axel Johansson <Axel Johansson>
2016-03-16 11:36:27 +0200

    Sofokus lisätty

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -10,6 +10,7 @@ Header = React.createClass({
             Menossa mukana:
             <a href="https://vaadin.com/" target="_blank" className="sponsor"> Vaadin</a>,
             <a href="http://www.nortal.fi/" target="_blank" className="sponsor"> Nortal</a>,
+            <a href="https://www.sofokus.com/" target="_blank" className="sponsor"> Sofokus</a>,
             <a href="https://www.tek.fi/" target="_blank" className="sponsor"> TEK</a>
           </li>
         </ul>
--- client/components/SponsorComponent.jsx
+++ client/components/SponsorComponent.jsx
@@ -10,6 +10,9 @@ SponsorComponent = React.createClass({
 					<a href="http://www.nortal.fi/" target="_blank"><img src="/images/sponsorit/Nortal.png"></img></a>
 				</div>
 				<div className="sponsor">
+					<a href="https://www.sofokus.com/" target="_blank"><img src="/images/sponsorit/Sofokus.png"></img></a>
+				</div>
+				<div className="sponsor">
 					<a href="https://www.tek.fi/" target="_blank"><img src="/images/sponsorit/TEK.png"></img></a>
 				</div>
 			</div>
